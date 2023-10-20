066f0ae89bca53e1a109c66239236d548b755d76
Phelipe Pereira <phelipe.pereira0811@hotmail.com>
2023-10-20 15:50:50 -0300

    Resolvendod desafio listaTarefa e carrinhoDeCompras
    Signed-off-by: Phelipe Pereira <phelipe.pereira0811@hotmail.com>

---
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,71 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    public class CarrinhoDeCompras {
+
+    private List<Item> comprasList;
+
+    public CarrinhoDeCompras(){
+        this.comprasList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quant){
+        Item item = new Item(nome, preco, quant);
+        comprasList.add(item);
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for (Item i: comprasList){
+            if(i.getNome().equalsIgnoreCase(nome)){
+                itensParaRemover.add(i);
+            }
+        }
+        comprasList.removeAll(itensParaRemover);
+    }
+
+    public double calcularValorTotal(){
+        double valorTotal = 0d;
+        for (Item i: comprasList){
+            double valorItem = i.getPreco()*i.getQuant();
+            valorTotal += valorItem;
+        }
+        return valorTotal;
+    }
+
+    public void exibirItens() {
+        System.out.println(this.comprasList);
+    }
+
+        @Override
+        public String toString() {
+            return "CarrinhoDeCompras{" +
+                    "comprasList=" + comprasList +
+                    '}';
+        }
+
+        public static void main(String[] args) {
+            // Criando uma instância do carrinho de compras
+            CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+            // Adicionando itens ao carrinho
+            carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+            carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+            carrinhoDeCompras.adicionarItem("Caderno", 35d, 1);
+            carrinhoDeCompras.adicionarItem("Borracha", 2d, 2);
+
+            // Exibindo os itens no carrinho
+            carrinhoDeCompras.exibirItens();
+
+            // Removendo um item do carrinho
+            carrinhoDeCompras.removerItem("Lápis");
+
+            // Exibindo os itens atualizados no carrinho
+            carrinhoDeCompras.exibirItens();
+
+            // Calculando e exibindo o valor total da compra
+            System.out.println("O valor total da compra é = " + carrinhoDeCompras.calcularValorTotal());
+        }
+    }
